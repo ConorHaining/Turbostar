3ee01af6d6f21b14c5f78ff3090131960f3ccf30
Conor Haining <conor.haining@gmail.com>
2018-05-03 08:16:19 +0100

    Skeleton tests for feature

---
+++ tests/Feature/ParseScheduleTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ParseScheduleTest extends TestCase
+{
+
+    protected function setUp()
+    {
+      parent::setUp();
+
+      mkdir(__DIR__.'/../ParseScheduleTestTemp');
+    }
+
+
+    public function testFullFileDownload()
+    {
+        $command = $this->artisan('parse:schedule', ['--full' => true]);
+
+        // dd($command);
+    }
+
+    public function testDailyFileDownload()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function tearDown()
+    {
+      parent::tearDown();
+
+      exec('rm -rf '.__DIR__.'/../ParseScheduleTestTemp');
+    }
+}
