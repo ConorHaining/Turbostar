7f381c5e395623d7b366d4ef5994fff5d73b3923
Conor Haining <conor.haining@gmail.com>
2018-05-29 16:30:33 +0100

    New Tiploc Model

---
+++ app/TipclocModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Jenssegers\Mongodb\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\SoftDeletes;
+
+class TipclocModel extends Model
+{
+  use SoftDeletes;
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   * @see https://wiki.openraildata.com/index.php/Tiploc_Records
+   */
+  protected $fillable = [
+    'code',
+    'nalco',
+    'stanox',
+    'crs',
+    'description',
+    'name'
+  ];
+}
