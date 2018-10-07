0f176563bc2f3ed42339819f2a54bf722ebf3687
Conor Haining <conor.haining@gmail.com>
2018-10-07 18:15:36 +0100

    Default to the mysql connection

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
