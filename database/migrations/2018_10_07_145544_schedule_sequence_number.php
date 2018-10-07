f286396534d67b8076cb356b43df4d06fe6222d6
Conor Haining <conor.haining@gmail.com>
2018-10-07 17:55:20 +0100

    Sequence migration

---
+++ database/migrations/2018_10_07_145544_schedule_sequence_number.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ScheduleSequenceNumber extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedule_sequence', function (Blueprint $table) {
+            $table->integer('sequence');
+            $table->timestamp('added_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedule_sequence');
+    }
+}
