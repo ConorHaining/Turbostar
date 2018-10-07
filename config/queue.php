9f0ad6b13889d58ad698194cda571fd58cc60a27
Conor Haining <conor.haining@gmail.com>
2018-10-07 17:45:06 +0100

    Use the correct queue driver

--- config/queue.php
+++ config/queue.php
@@ -35,7 +35,7 @@ return [
         ],
 
         'database' => [
-            'driver' => 'mysql',
+            'driver' => 'database',
             'table' => 'jobs',
             'queue' => 'default',
             'retry_after' => 90,
