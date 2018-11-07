f83d3067187868a9a3dffd88191a20ac9fbd576c
Conor Haining <conor.haining@gmail.com>
2018-11-07 13:36:11 +0000

    Expand demons

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -6,4 +6,6 @@ php artisan up
 php artisan cache:clear
 
 php artisan queue:work --queue=movement-0003,movement-0001,movement-0002,movement-0005,movement-0006,movement-0007,movement-0008 &
-php artisan queue:work --queue=schedule-create,schedule-delete,association-create,association-delete,tiploc-create,tiploc-update,tiploc-delete &
+php artisan queue:work --queue=schedule-create,schedule-delete &
+php artisan queue:work --queue=association-create,association-delete &
+php artisan queue:work --queue=tiploc-create,tiploc-update,tiploc-delete &
