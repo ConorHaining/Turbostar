2ea37ecc24c36103a2bb6c4bddfed6a6ef830ef0
Conor Haining <conor.haining@gmail.com>
2018-11-08 08:28:37 +0000

    Add deployment scripts

---
+++ scripts/ApplicationStart.sh
@@ -0,0 +1,11 @@
+# #!/usr/bin/env bash
+
+# cd /var/www/html
+
+# php artisan up
+# php artisan cache:clear
+
+# php artisan queue:work --queue=movement-0003,movement-0001,movement-0002,movement-0005,movement-0006,movement-0007,movement-0008 &
+# php artisan queue:work --queue=schedule-create,schedule-delete &
+# php artisan queue:work --queue=association-create,association-delete &
+# php artisan queue:work --queue=tiploc-create,tiploc-update,tiploc-delete &
