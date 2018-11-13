7995b3d2381703e7314249a4be6fef6ac994851d
Conor Haining <conor.haining@gmail.com>
2018-11-13 14:24:46 +0000

    put logs into dev null

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -5,7 +5,7 @@ cd /var/www/html
 php artisan up
 php artisan cache:clear
 
-php artisan queue:work --queue=movement-0003,movement-0001,movement-0002,movement-0005,movement-0006,movement-0007,movement-0008 &
-php artisan queue:work --queue=schedule-create,schedule-delete &
-php artisan queue:work --queue=association-create,association-delete &
-php artisan queue:work --queue=tiploc-create,tiploc-update,tiploc-delete &
+php artisan queue:work --queue=movement-0003,movement-0001,movement-0002,movement-0005,movement-0006,movement-0007,movement-0008 & > /dev/null
+php artisan queue:work --queue=schedule-create,schedule-delete & > /dev/null
+php artisan queue:work --queue=association-create,association-delete & > /dev/null
+php artisan queue:work --queue=tiploc-create,tiploc-update,tiploc-delete & > /dev/null
