f9403e46a3a32672220e33903aed93f603021646
Conor Haining <conor.haining@gmail.com>
2018-11-13 15:58:50 +0000

    Hook scripts

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,8 +1,9 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-# cd /var/www/html
+cd /var/www/html
 
-# php artisan cache:clear
-# rm -rf storage/app/schedule/*
+php artisan cache:clear
+rm -rf storage/app/schedule/*
+rm -rf storage/framework/cache/*
 
-# php artisan down
+php artisan down
