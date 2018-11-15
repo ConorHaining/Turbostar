599eadde3a4087bf35691c2780a52bdb49879dc0
Conor Haining <conor.haining@gmail.com>
2018-11-15 22:14:22 +0000

    Supervisor restart changes

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -4,4 +4,6 @@ cd /var/www/html
 
 php artisan up
 supervisord -c scripts/supervisord.conf
+supervisorctl -c scripts/supervisord.conf reread
+supervisorctl -c scripts/supervisord.conf update
 supervisorctl -c scripts/supervisord.conf start all
