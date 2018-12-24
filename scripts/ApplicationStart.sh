098e39ddac2eb6877f310aa4257fa832eaa6e3e7
Conor Haining <conor.haining@gmail.com>
2018-12-24 21:30:22 +0000

    New build process and supervisor tasks

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -7,3 +7,4 @@ supervisord -c scripts/supervisord.conf
 supervisorctl -c scripts/supervisord.conf reread
 supervisorctl -c scripts/supervisord.conf update
 supervisorctl -c scripts/supervisord.conf start all
+php artisan horizon
