909ce6029b1f9c566da9c0874977f9e5de4a4334
Conor Haining <conor.haining@gmail.com>
2018-12-24 21:30:35 +0000

    We don't need that

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -7,4 +7,3 @@ supervisord -c scripts/supervisord.conf
 supervisorctl -c scripts/supervisord.conf reread
 supervisorctl -c scripts/supervisord.conf update
 supervisorctl -c scripts/supervisord.conf start all
-php artisan horizon
