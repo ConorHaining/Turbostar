5217bbd40bb53d45539a27408ff2dd93ed2db9e4
Conor Haining <conor.haining@gmail.com>
2019-01-03 15:43:16 +0000

    No longer terminate Horizon

--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -4,6 +4,5 @@ cd /var/www/html
 
 php artisan down
 php artisan stomp:stop
-php artisan horizon:terminate
 sleep 15
-supervisorctl -c scripts/supervisord.conf stop all
+supervisorctl -c scripts/supervisord.conf stop stomp:*
