0e810b9e537745fa3ae51979117a31d508d451ff
Conor Haining <conor.haining@gmail.com>
2019-01-03 15:43:44 +0000

    Change storage permission

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -7,3 +7,4 @@ supervisord -c scripts/supervisord.conf
 supervisorctl -c scripts/supervisord.conf reread
 supervisorctl -c scripts/supervisord.conf update
 supervisorctl -c scripts/supervisord.conf start all
+sudo chmod 777 -R storage/
